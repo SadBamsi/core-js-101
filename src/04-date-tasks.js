e944907cd0196d409f05b700e1ae267c70ac23c3
Bamsi-home <omaslov@xbsoftware.by>
2020-12-14 22:08:50 +0300

    dev: add solutions to dates tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -18,8 +18,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -33,8 +33,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 /**
@@ -51,8 +51,8 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  return new Date(new Date(date).setDate(29)).getMonth() === 1;
 }
 
 /**
@@ -70,8 +70,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  return `${(`0${endDate.getHours() - startDate.getHours()}`).slice(-2)}:${(`0${endDate.getMinutes() - startDate.getMinutes()}`).slice(-2)}:${(`0${endDate.getSeconds() - startDate.getSeconds()}`).slice(-2)}.${(`00${endDate.getMilliseconds() - startDate.getMilliseconds()}`).slice(-3)}`;
 }
 
 /**
@@ -90,8 +90,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hourArrow = 60 * (date.getUTCHours() % 12);
+  const minutesArrow = 11 * date.getUTCMinutes();
+  const angle = Math.abs(0.5 * (hourArrow - minutesArrow)) > 180
+    ? 360 - Math.abs(0.5 * (hourArrow - minutesArrow))
+    : Math.abs(0.5 * (hourArrow - minutesArrow));
+
+  return angle * (Math.PI / 180);
 }
 
 module.exports = {
